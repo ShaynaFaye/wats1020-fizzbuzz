7973a0b75ed1ea7b0824fc485040f25016d056fd
worthensf <worthensf@gmail.com>
2016-01-11 03:23:50 -0500

    completed project

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -2,12 +2,13 @@
 
 var currentNumber = 0;
 
+//go through numbers 1-20 and determine whether each number is divisible by 3, 5, or bothh
 for(var i = 0; i < 20; i++) {
 	//increment current number by 1
     currentNumber = currentNumber + 1;
 	//check if current number is divisible by 3 and 5
     if (currentNumber % 3 === 0 && currentNumber % 5 === 0) {
-	//if current number is divisible by 3 and 5 then display word fizzbuzz on consolee
+	//if current number is divisible by 3 and 5 then display word fizzbuzz on console
     console.log ("FizzBuzz");
 		//check if current number is divisible by 3
 	} else  if (currentNumber % 3 === 0) {
